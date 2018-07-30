282daa4ab90fb5d3b8c0d321ad86e73605b51b7a
Adam Urbina <adamurbina@gmail.com>
2018-07-30 19:52:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     resp = Rack::Response.new
 
 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
+    num_1 = Kernel.rand(1..10)
+    num_2 = Kernel.rand(1..10)
+    num_3 = Kernel.rand(1..10)
 
     if num_1 == num_2 && num_2 == num_3
         resp.write num_1
